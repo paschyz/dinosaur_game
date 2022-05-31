0a9f126a794559b8b7eea5493d3327653e50ddb2
pascal <paschy2002@gmail.com>
2022-05-31 22:42:55 +0200

    adjusted alert box message

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ var checkDead = setInterval(function () {
   );
   if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
     block.style.animation = "none";
-    alert("u lose.");
+    alert("You lost !");
   }
 }, 10);
